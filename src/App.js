26953c7308271f51e29b20119023e8961b7651fd
Harshad <harshadchavan0005@gmail.com>
2023-05-26 10:08:29 +0530

    final Edit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import { feature } from './feature';
+import { useState ,useRef} from 'react';
+import phone from './phone.png'
+
 import './App.css';
 
 function App() {
+  const startItem = useRef();
+  const dragOverItem = useRef();
+  const [list, setList] = useState(feature);
+ 
+  const dragStart = (e, position) => {
+    startItem.current = position;
+    console.log(startItem.current)
+    console.log(e.target.innerHTML);
+  };
+ 
+  const dragEnter = (e, position) => {
+    dragOverItem.current = position;
+    console.log(dragOverItem.current);
+    console.log(e.target.innerHTML);
+  };
+ 
+  const drop = (e) => {
+    const copyListItems = [...list];
+    const dragItemContent = copyListItems[startItem.current];
+    const that=copyListItems[dragOverItem.current];
+    console.log(that)
+    console.log(dragItemContent)
+    copyListItems.splice(startItem.current, 1,that);
+   // console.log( copyListItems.splice(startItem.current, 1))
+   //copyListItems.splice(startItem.current,0,that);
+    copyListItems.splice(dragOverItem.current, 1, dragItemContent);
+  //  copyListItems.splice(dragOverItem.current, 1);
+  
+    startItem.current = null;
+    dragOverItem.current = null;
+    console.log(copyListItems);
+    setList(copyListItems);
+  };
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <img className='immg' src={phone} alt='hello'></img>
+    <div className='divv'>
+    {
+    list&&
+    list.map((item, index) => (
+    
+      <div className={`div div${index} `} 
+        onDragStart={(e) => dragStart(e, index)}
+        onDragEnter={(e) => dragEnter(e, index)}
+        onDragEnd={drop}
+        key={index}
+        draggable>
+         <h5 className='title'>{item.title}</h5> 
+          <p className='desc'>{item.Description}</p>
+      </div>
+      ))}
     </div>
+    </>
   );
 }
 
---
+++ src/feature.js
@@ -0,0 +1,18 @@
+export const feature=[{title:"Feature 1",
+Description:"SMS and MMS are available forms of messaging, including threaded text messaging "}
+,{title:"Feature 2",
+Description:"GSM/EDGE, IDEN, CDMA, EV-DO, UMTS, Bluetooth, Wi-Fi, LTE, NFC and WiMAX."}
+,{title:"Feature 3",
+Description:"SQLite, a lightweight relational database, is used for data storage purposes."}
+,{title:"Feature 4",
+Description:"H.263, H.264, MPEG-4 SP, AMR, AMR-WB, AAC, HE-AAC, AAC 5.1, MP3, MIDI, Ogg Vorbis, WAV, JPEG, PNG, GIF, and BM"}
+,{title:"Feature 5",
+Description:"Based on the open-source WebKit layout engine, coupled with Chrome's V8 JavaScript engine supporting HTML5 and CSS3"}
+,{title:"Feature 6",
+Description:"User can jump from one task to another and same time various application can run simultaneously."}
+,
+{title:"Feature 7",
+Description:"Android has native support for multi-touch which was initially made available in handsets such as the HTC Hero."},
+{title:"Feature 8",
+Description:"Widgets are resizable, so users can expand them to show more content or shrink them to save space."}
+]
